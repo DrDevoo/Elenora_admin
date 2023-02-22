d87f6433635d8fa7f8fd3d4984d85e160d13387b
DrDevoo <krichard001@icloud.com>
2023-02-22 22:40:55 +0100

    upload

---
+++ src/components/Header.vue
@@ -0,0 +1,495 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      showMenu: false,
+      showCart: false,
+      showSearch: false,
+      showWish: false,
+      header_title: "null",
+      cart: [],
+      imgurl: import.meta.env.VITE_API_URL + "/getimage/",
+
+      sitestatus: "online"
+    };
+  },
+  mounted() {
+    axios
+      .get(import.meta.env.VITE_API_URL + "/settings/get/header_title")
+      .then((response) => (this.header_title = response.data));
+    axios
+      .get(import.meta.env.VITE_API_URL + "/settings/get/sitestatus")
+      .then((response) => (
+        this.sitestatus = response.data.value
+        ));
+
+  },
+  created() {
+    this.cart = JSON.parse(localStorage.getItem("cart") || "[]");
+  },
+  computed: {
+    total() {
+      return this.cart.reduce(
+        (sum, item) => sum + item.price * item.quantity,
+        0
+      );
+    },
+  },
+  methods: {
+    removeItem(index) {
+      this.cart.splice(index, 1);
+      localStorage.setItem("cart", JSON.stringify(this.cart));
+    },
+    openCart() {
+      this.cart = JSON.parse(localStorage.getItem("cart") || "[]");
+      this.showCart = !this.showCart;
+    },
+    checkout() {},
+  },
+};
+</script>
+
+<template>
+  <div class="info">
+    <div>{{ this.header_title.value }}</div>
+  </div>
+  <header>
+    <div class="header_wrapper">
+      <div>
+        <a @click="showMenu = !showMenu"
+          ><ion-icon name="menu-outline"></ion-icon
+        ></a>
+        <a @click="showSearch = !showSearch">
+          <ion-icon name="search-outline"></ion-icon>
+        </a>
+      </div>
+      <div>
+        <RouterLink to="/"
+          ><img class="logo" src="../assets/images/logo/logo.svg" alt="ELENORA"
+        /></RouterLink>
+      </div>
+      <div>
+        <a @click="showWish = !showWish"
+          ><ion-icon name="heart-outline"></ion-icon
+        ></a>
+        <a @click="showCart = !showCart"
+          ><ion-icon name="bag-outline"></ion-icon
+        ></a>
+      </div>
+    </div>
+  </header>
+
+  <section v-if="showMenu" class="menu_wrapper" id="menuBtn">
+    <a @click="showMenu = !showMenu"
+      ><ion-icon name="close-outline"></ion-icon
+    ></a>
+    <br />
+    <div class="menu-link-list">
+      <h3 class="linktitle">Ékszerek</h3>
+      <RouterLink to="/shop/women"
+        ><h3 class="link">Női karkötők</h3></RouterLink
+      >
+      <RouterLink to="/shop/men"
+        ><h3 class="link">Férfi karkötők</h3></RouterLink
+      >
+      <RouterLink to="/shop/couple"
+        ><h3 class="link">Páros karkötők</h3></RouterLink
+      >
+      <RouterLink to="/shop/sets"
+        ><h3 class="link">Szettek</h3></RouterLink
+      >
+      <RouterLink class="flexlink" to="/shop/sales"
+        ><h3 class="link">Akciós karkötők</h3><h6>Új kedvezmények!</h6></RouterLink
+      >
+      <RouterLink to="/shop"><h3 class="link">Összes karkötő</h3></RouterLink>
+
+      <h3 class="linktitle">Kiegészítők</h3>
+      <RouterLink class="flexlink" to="/shop/boravia"
+        ><h3 class="link">Fonott karkötők</h3> <h6>Készlet erejéig!</h6></RouterLink
+      >
+      <RouterLink to="/shop/accessories"
+        ><h3 class="link">Kulcstartók</h3></RouterLink
+      >
+      <RouterLink to="/asvany-katalogus"
+        ><h3 class="linktitle">Ásvány Lexikon</h3></RouterLink
+      >
+      <RouterLink to="/kapcsolat"
+        ><h3 class="linktitle">Kapcsolat</h3></RouterLink
+      >
+      <RouterLink to="/gyik"><h3 class="linktitle">Gy.i.k</h3></RouterLink>
+    </div>
+  </section>
+
+  <section v-if="showCart" class="cart_wrapper" id="cartBtn">
+    <a @click="openCart()"
+      ><ion-icon class="cart_close" name="close-outline"></ion-icon
+    ></a>
+    <h1>Kosár ({{ cart.length }})</h1>
+    <div class="cart_content">
+      <div class="cart_item" v-for="(item, index) in cart" :key="index">
+        <div class="cart_item_imgtext">
+          <div class="cart_item_img">
+            <img :src="imgurl + item.img" />
+          </div>
+          <div class="cart_item_desc">
+            <p class="prodname">{{ item.name }}</p>
+            <p class="size">Méret: {{ item.size }}</p>
+            <div class="quantity_b">
+              <ion-icon class="i" name="add-outline" @click="addq()"></ion-icon>
+              <input type="number" min="1" max="15" v-model="item.quantity" />
+              <ion-icon
+                class="i"
+                name="remove-outline"
+                @click="minq()"
+              ></ion-icon>
+            </div>
+          </div>
+        </div>
+        <div class="cart_item_del">
+          <div>
+            <ion-icon
+              class="cart_close"
+              name="close-outline"
+              @click="removeItem(index)"
+            ></ion-icon>
+          </div>
+          <br /><br /><br />
+          <div>
+            <h5>{{ item.price }} Ft</h5>
+          </div>
+        </div>
+      </div>
+
+      <div class="bottom">
+        <p class="total">Termékek ára: {{ total }} Ft</p>
+        <RouterLink to="/shop/checkout"
+          ><div class="nextbtn"><p>Fizetés</p></div></RouterLink
+        >
+      </div>
+    </div>
+  </section>
+
+  <section v-if="showWish" class="cart_wrapper" id="cartBtn">
+    <a @click="showWish = !showWish"
+      ><ion-icon name="close-outline"></ion-icon
+    ></a>
+    <h1>Kívánságaim (0)</h1>
+  </section>
+
+  <section v-if="showSearch" class="menu_wrapper" id="cartBtn">
+    <a @click="showSearch = !showSearch"
+      ><ion-icon name="close-outline"></ion-icon
+    ></a>
+    <h1>Keresés</h1>
+  </section>
+</template>
+
+<style scoped>
+.flexlink{
+  width: 330px;
+  height: 30px;
+display: flex;
+align-items: center;
+}
+.flexlink h6{
+  letter-spacing: 1px;
+  color: white;
+  width: 170px;
+  background-color: lightsalmon;
+  padding: 4px;
+  text-align: center;
+  border-radius: 6px;
+}
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+
+.quantity_b {
+  padding: 0.2rem;
+  border-radius: 10px;
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: rgba(206, 206, 206, 0.497);
+}
+.quantity_b .i {
+  font-size: 13pt;
+}
+.quantity_b input {
+  background: transparent;
+  border: none;
+  text-align: center;
+  font-size: 10pt;
+  width: fit-content;
+}
+.cart_item {
+  height: 130px;
+  padding: 1rem;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid gray;
+  width: 90%;
+  align-items: center;
+}
+.cart_item_imgtext {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+.cart_item_img {
+}
+.cart_item_img img {
+  aspect-ratio: 1/1;
+  width: 100px;
+  border-radius: 10px;
+}
+.cart_item_desc {
+  position: relative;
+  top: -5px;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+}
+.cart_item_desc .prodname{
+  line-height: 15px;
+}
+.cart_item_desc .size{
+  line-height: 0px;
+}
+.cart_item_del {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
+}
+
+.bottom {
+  height: 80px;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  background-color: white;
+  z-index: 15;
+  box-shadow: 0px 0px 10px rgba(40, 40, 40, 0.484);
+  border-top: 1px solig rgba(128, 128, 128, 0.797);
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.total {
+  font-weight: bold;
+}
+.nextbtn {
+  background-color: rgb(17, 17, 17);
+  width: 150px;
+  border-radius: 5px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.nextbtn p {
+  color: white;
+  font-weight: bolder;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+}
+.info {
+  z-index: 10;
+  background-color: rgba(255, 160, 122, 0.895);
+  width: 100%;
+  height: 25px;
+  text-align: center;
+}
+.info div {
+  line-height: 30px;
+  color: white;
+  animation: roll 7s linear infinite;
+  position: relative;
+}
+@keyframes roll {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.header_wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+}
+
+header {
+  position: sticky;
+  top: 0;
+  padding: 0.5rem;
+  z-index: 10;
+  -webkit-backdrop-filter: blur(5px);
+  backdrop-filter: blur(5px);
+  height: 35px;
+}
+
+header ion-icon {
+  font-size: 18pt;
+  margin-left: 0.4rem;
+  margin-right: 0.4rem;
+  color: rgb(0, 0, 0);
+}
+
+.logo {
+  height: 1.5rem;
+  width: 200px;
+}
+
+.menu-link-list {
+  left: 0;
+  position: relative;
+}
+.menu_wrapper {
+  position: fixed;
+  height: 100vh;
+  width: 55%;
+  background-color: rgba(255, 255, 255, 0.982);
+  top: 0;
+  left: 0;
+  box-shadow: 0px 0px 10px rgba(84, 83, 83, 0.549);
+  z-index: 10;
+
+  animation-name: animMenu;
+  animation-duration: 0.5s;
+}
+.menu_wrapper h1 {
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: 200;
+  width: 100%;
+}
+.menu_wrapper ion-icon {
+  font-size: 2rem;
+  position: absolute;
+}
+.menu_wrapper a {
+  text-decoration: none;
+  color: black;
+}
+.menu-link-list a .link {
+  position: relative;
+  font-weight: 200;
+  width: 90%;
+  left: 2rem;
+  letter-spacing: 2px;
+}
+.linktitle {
+  position: relative;
+  font-weight: 200;
+  width: 90%;
+  left: 1rem;
+  letter-spacing: 1.5px;
+  text-transform: uppercase;
+}
+@keyframes animMenu {
+  from {
+    left: -400px;
+  }
+  to {
+    left: 0;
+  }
+}
+
+.cart_wrapper {
+  position: fixed;
+  height: 100%;
+  width: 55%;
+  background-color: rgba(255, 255, 255, 0.982);
+  top: 0;
+  right: 0;
+  box-shadow: 0px 0px 10px rgba(84, 83, 83, 0.549);
+  z-index: 10;
+
+  animation-name: anim;
+  animation-duration: 0.5s;
+}
+
+.cart_wrapper h1 {
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: 200;
+  width: 100%;
+}
+.cart_close {
+  font-size: 2rem;
+  position: absolute;
+}
+.cart_wrapper a {
+  text-decoration: none;
+  color: black;
+}
+@keyframes anim {
+  from {
+    right: -400px;
+  }
+  to {
+    right: 0;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  header {
+    padding: 0rem;
+  }
+
+  header ion-icon {
+    font-size: 16pt;
+    margin-left: 0.5rem;
+    color: rgb(0, 0, 0);
+  }
+
+  .logo {
+    height: 1.3rem;
+    width: 200px;
+  }
+  .info {
+    z-index: 10;
+    background-color: rgba(255, 160, 122, 0.895);
+    width: 100%;
+    height: 25px;
+    text-align: center;
+  }
+  .info div {
+    line-height: 30px;
+    color: white;
+    animation: roll 8s linear infinite;
+    position: relative;
+  }
+
+  .cart_wrapper {
+    width: 90%;
+  }
+  .menu_wrapper {
+    width: 90%;
+  }
+  .prodname{
+    line-height: 20px;
+  }
+}
+</style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,226 @@
+<script>
+export default {
+  data() {
+    return {
+      delay: 3500,
+      activeIndex: 0,
+      handler: null,
+      isPlaying: true,
+    };
+  },
+  methods: {
+    stop() {
+      this.isPlaying = false;
+      console.log("stop");
+      clearTimeout(this.handler);
+    },
+    start() {
+      this.isPlaying = true;
+      console.log("start");
+    },
+    handleClick(index) {
+      const slides = document.querySelectorAll(".slide");
+      const buttons = document.querySelectorAll(".button");
+
+      slides.forEach((slide) => {
+        slide.classList.remove("active");
+
+        buttons.forEach((btn) => {
+          btn.classList.remove("active");
+        });
+      });
+
+      this.activeIndex = index;
+
+      slides[this.activeIndex].classList.add("active");
+      buttons[this.activeIndex].classList.add("active");
+    },
+    autoPlay() {
+      const slides = document.querySelectorAll(".slide");
+      const buttons = document.querySelectorAll(".button");
+
+      let active = document.getElementsByClassName("active");
+
+      const repeater = () => {
+        [...active].forEach((activeSlide) => {
+          activeSlide.classList.remove("active");
+        });
+
+        slides[this.activeIndex].classList.add("active");
+        buttons[this.activeIndex].classList.add("active");
+
+        this.activeIndex++;
+
+        if (slides.length === this.activeIndex) {
+          this.activeIndex = 0;
+        }
+
+        if (this.activeIndex >= slides.length) {
+          return;
+        }
+
+        this.handler = setTimeout(() => {
+          repeater();
+        }, this.delay);
+      };
+
+      repeater();
+    },
+  },
+  mounted() {
+    const buttons = document.querySelectorAll(".button");
+
+    buttons.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        this.handleClick(index);
+      });
+    });
+
+    this.autoPlay();
+  },
+};
+</script>
+
+<template>
+  <div class="slider-wrapper">
+    <div class="slides">
+      <div class="slide active">
+        <img src="../assets/images/webp/1.webp" />
+        <h2>Páros karkötő</h2>
+        <p>Oszd meg ásvány karkötődet barátoddal, pároddal</p>
+      </div>
+      <div class="slide">
+        <img src="../assets/images/webp/2.webp" />
+        <h2>Változatosság</h2>
+        <p>Válassz olyan karkötőt amiben megmutatkozik a sok színűséged</p>
+      </div>
+      <div class="slide">
+        <img src="../assets/images/webp/3.webp" />
+        <h2>Elegáns</h2>
+        <p>Hord egy ünnepélyes eseményhez, egészítse ki az öltözéked</p>
+      </div>
+      <div class="slide">
+        <img src="../assets/images/webp/4.webp" />
+        <h2>Mindennapokban</h2>
+        <p>Viseld a szürke hétköznapokban, hogy feldobják a kedved</p>
+      </div>
+    </div>
+    <div class="controls">
+      <button class="button active"></button>
+      <button class="button"></button>
+      <button class="button"></button>
+      <button class="button"></button>
+    </div>
+  </div>
+</template>
+
+<style>
+.slides {
+  position: relative;
+  height: 500px;
+  width: 100vw;
+}
+
+.slide {
+  display: flex;
+  position: absolute;
+  width: 100%;
+  height: 500px;
+  opacity: 1;
+  z-index: 1;
+  color: #000;
+  text-align: center;
+  background-color: lightsalmon;
+}
+.slide h2 {
+  color: white;
+  letter-spacing: 3px;
+  font-size: 30pt;
+  position: absolute;
+  top: 35%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  filter: drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.535));
+}
+.slide p {
+  color: white;
+  letter-spacing: 3px;
+  font-size: 20pt;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  filter: drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.535));
+  transform: translate(-50%, -50%);
+}
+
+.slide img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.slide.active {
+  opacity: 1;
+  z-index: 2;
+  animation: slide 0.6s ease;
+}
+
+@keyframes slide {
+  from {
+    left: -100%;
+  }
+  to {
+    left: 0;
+  }
+}
+
+.controls {
+  z-index: 3;
+  position: relative;
+  top: -2rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  height: 10px;
+}
+
+.button {
+  appearance: none;
+  width: 1px;
+  height: 4px;
+  background-color: rgba(0, 0, 0, 0.54);
+  outline: none;
+  border: none;
+  margin: 0 5px;
+  border-radius: 20px;
+}
+
+.button.active {
+  background-color: rgba(0, 0, 0, 0.667);
+  width: 30px;
+  animation: wid 0.8s ease;
+}
+@keyframes wid {
+  from {
+    width: 0px;
+  }
+  to {
+    width: 30px;
+  }
+}
+
+@media only screen and (max-width: 1000px) {
+  .slide h2 {
+    letter-spacing: 3px;
+    font-size: 25pt;
+    top: 38%;
+    width: 100%;
+  }
+  .slide p {
+    letter-spacing: 3px;
+    font-size: 15pt;
+    width: 95%;
+    top: 50%;
+  }
+}
+</style>
