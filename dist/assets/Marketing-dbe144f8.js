85481edf9335b6f7c8fcb25b1e95a0f9fe6d87df
DrDevoo <krichard001@icloud.com>
2023-02-24 16:18:03 +0100

    update

--- dist/assets/Marketing-dbe144f8.js
+++
@@ -1,1 +0,0 @@
-import{H as t}from"./Header-562980d7.js";import{o as a,c as s,e as n,F as _,a as e}from"./index-271672bb.js";import"./_plugin-vue_export-helper-c27b6911.js";const o=e("h1",{class:"title"},"Márketing",-1),r=e("main",null,null,-1),c={data(){return{showAdd:!1}}},f=Object.assign(c,{__name:"Marketing",setup(l){return(i,d)=>(a(),s(_,null,[n(t),o,r],64))}});export{f as default};
--- src/views/admin/components/OrdersList.vue
+++ src/views/admin/components/OrdersList.vue
@@ -8,7 +8,6 @@ export default {
       p_order: [],
       details: false,
       imgurl: import.meta.env.VITE_API_URL + "/getimage/",
-
     };
   },
   mounted() {
@@ -76,59 +75,78 @@ export default {
   </section>
   <section class="details_box" v-if="details">
     <div class="d_container">
-      <ion-icon @click="details = !details" style="font-size: 24pt" name="close-outline"></ion-icon>
-      <h3 style="text-align: center">Rendekés összesítő - <i>{{ this.p_order.orderid }}</i></h3>
+      <ion-icon
+        @click="details = !details"
+        style="font-size: 24pt; position: sticky; top: 0"
+        name="close-outline"
+      ></ion-icon>
+      <h3 style="text-align: center">
+        Rendekés összesítő - <i>{{ this.p_order.orderid }}</i>
+      </h3>
       <div class="customer_b">
         <div class="box">
           <h5>Megrendelő adatai:</h5>
           <div class="flex">
-            <b><p>Teljes név:</p></b> <p>{{ this.p_order.u_firstname }} {{ this.p_order.u_name }}</p>
+            <b><p>Teljes név:</p></b>
+            <p>{{ this.p_order.u_firstname }} {{ this.p_order.u_name }}</p>
           </div>
           <div class="flex">
-            <b><p>E-mail:</p></b> <p>{{ this.p_order.u_email }}</p>
+            <b><p>E-mail:</p></b>
+            <p>{{ this.p_order.u_email }}</p>
           </div>
           <div class="flex">
-            <b><p>Tel.:</p></b> <p>{{ this.p_order.u_tel }}</p>
+            <b><p>Tel.:</p></b>
+            <p>{{ this.p_order.u_tel }}</p>
           </div>
         </div>
         <div class="box">
           <h5>Szállítás, számlázás:</h5>
           <div class="flex">
-            <b><p>helység:</p></b> <p>{{ this.p_order.u_postnumber }}, {{ this.p_order.u_city }}</p>
+            <b><p>helység:</p></b>
+            <p>{{ this.p_order.u_postnumber }}, {{ this.p_order.u_city }}</p>
           </div>
           <div class="flex">
-            <b><p>Utca:</p></b> <p>{{ this.p_order.u_addresse }}</p>
+            <b><p>Utca:</p></b>
+            <p>{{ this.p_order.u_addresse }}</p>
           </div>
         </div>
         <div class="box">
           <h5>Rendelés adatai:</h5>
           <div class="flex" v-if="this.p_order.createdtime">
-            <b><p>Idő:</p></b> <p>{{ this.p_order.createdtime.split("T")[0] }}</p>
+            <b><p>Idő:</p></b>
+            <p>{{ this.p_order.createdtime.split("T")[0] }}</p>
           </div>
           <div class="flex">
-            <b><p>Státusz:</p></b> <p></p>
+            <b><p>Státusz:</p></b>
+            <p></p>
           </div>
         </div>
       </div>
       <div>
-        <h4 style="text-align: center;">Rendelt termékek</h4>
-        <div class="cartprod" v-for="(item, index) in this.p_order.cart" :key="index">
-          <img class="prodimg" :src="imgurl + item.img">
+        <h4 style="text-align: center">Rendelt termékek</h4>
+        <div
+          class="cartprod"
+          v-for="(item, index) in this.p_order.cart"
+          :key="index"
+        >
+          <img class="prodimg" :src="imgurl + item.img" />
+          <div></div>
           <div>
-            
-          </div>
-          <div>
             <div class="flex">
-              <b><p>Neve:</p></b> <p>{{ item.name }}</p>
+              <b><p>Neve:</p></b>
+              <p>{{ item.name }}</p>
             </div>
             <div class="flex">
-              <b><p>Mennyiség:</p></b> <p>{{ item.quantity }}</p>
+              <b><p>Mennyiség:</p></b>
+              <p>{{ item.quantity }}</p>
             </div>
             <div class="flex">
-              <b><p>Méret:</p></b> <p>{{ item.size }}</p>
+              <b><p>Méret:</p></b>
+              <p>{{ item.size }}</p>
             </div>
             <div class="flex">
-              <b><p>Ára:</p></b> <p>{{ item.price }} Ft</p>
+              <b><p>Ára:</p></b>
+              <p>{{ item.price }} Ft</p>
             </div>
           </div>
         </div>
@@ -138,29 +156,30 @@ export default {
 </template>
 
 <style scoped>
-.cartprod{
+.cartprod {
   display: flex;
   margin-bottom: 1rem;
-  gap:  0.5rem;
+  gap: 0.5rem;
 }
-.customer_b{
+.customer_b {
   display: flex;
   justify-content: space-between;
   border: 1px solid gray;
   border-radius: 10px;
 }
-.prodimg{
+.prodimg {
   width: 90px;
+  border-radius: 5px;
 }
-.customer_b .box{
+.customer_b .box {
   padding: 0.4rem;
 }
-.flex{
+.flex {
   height: 30px;
   display: flex;
   gap: 0.5rem;
 }
-.box h5{
+.box h5 {
   line-height: 0;
 }
 .details_box {
@@ -174,6 +193,7 @@ export default {
   align-items: center;
 }
 .details_box .d_container {
+  overflow-y: auto;
   box-shadow: 0px 0px 10px black;
   width: 70%;
   height: 80vh;
@@ -212,5 +232,24 @@ th,
 td {
   text-align: center;
   border-bottom: 1px solid gray;
+}
+
+@media only screen and (max-width: 900px) {
+  .details_box .d_container {
+  overflow-y: auto;
+  box-shadow: 0px 0px 10px black;
+  width: 95%;
+  height: 83vh;
+  background-color: #ffffff;
+  border-radius: 10px;
+  padding: 1rem;
+}
+.customer_b {
+  display: block;
+  justify-content: space-between;
+  border: 1px solid gray;
+  border-radius: 10px;
+  gap: 1rem;
+}
 }
 </style>
