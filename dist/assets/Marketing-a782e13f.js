91aac431884b00603ebc547b0541a111814364df
DrDevoo <krichard001@icloud.com>
2023-02-26 23:28:47 +0100

    change

--- dist/assets/Marketing-a782e13f.js
+++
@@ -1,1 +0,0 @@
-import{H as t}from"./Header-a51dc601.js";import{o as a,c as s,e as n,F as _,a as e}from"./index-434e6c56.js";import"./_plugin-vue_export-helper-c27b6911.js";const o=e("h1",{class:"title"},"Márketing",-1),r=e("main",null,null,-1),c={data(){return{showAdd:!1}}},f=Object.assign(c,{__name:"Marketing",setup(l){return(i,d)=>(a(),s(_,null,[n(t),o,r],64))}});export{f as default};
